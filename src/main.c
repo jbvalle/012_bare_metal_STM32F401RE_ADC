0195c59708d4f3d13099f2daf5c0a93d8d08b5c7
John Bryan Valle <jbvalle.jv@gmail.com>
2022-11-03 12:52:44 +0100

    implemented adc conversion using interrupt

--- src/main.c
+++ src/main.c
@@ -13,6 +13,9 @@
 #define LENGTH_SEQ 20
 #define CONV_START 30
 #define EOC_FLAG 1
+#define EOC_INTERRUPT 5
+#define ADC_IRQ 18
+#define CONT_MODE 1
 
 ADC_t   * const ADC    = (ADC_t    *)  0x40012000;
 RCC_t   * const RCC     = (RCC_t    *)  0x40023800;
@@ -28,11 +31,30 @@ void wait_ms(int time){
     }
 }
 
+void _global_enable_NVIC(void){
+    uint32_t priMASK = 0;
+    __asm volatile("MSR primask, %0"::"r"(priMASK):"memory");
+}
+
+void _global_disable_NVIC(void){
+    uint32_t priMASK = 1;
+    __asm volatile("MSR primask, %0"::"r"(priMASK):"memory");
+}
+
+
+void ADC_handler(void){
+
+    // Read ADC value | EOC is reset automatically
+    uint16_t result = ADC->ADC_DR;
+    printf("Value %u:\r\n", (uint16_t)result);
+}
+
 int main(void){
 
     initialise_monitor_handles();
 
-
+    // SET global disable 
+    _global_disable_NVIC();
     // Enable and Configure GPIOA P5 
     RCC->RCC_AHB1ENR |= GPIOA_EN;
     
@@ -49,22 +71,23 @@ int main(void){
     RCC->RCC_APB2ENR |= (1 << ADC1);
     // Turn off ADC for configuration
     ADC->ADC_CR2 &= ~1;
-
     // Set Total number of convers to 1 | setting to 0 = 1 conversion
     ADC->ADC_SQR1 &= ~(0xF << LENGTH_SEQ);
     // Set first conversion in SQR to be from CH1
     ADC->ADC_SQR3 &= ~0x1F;
     ADC->ADC_SQR3 |= 1;
+    // Enable interrupt for EOC FLAG
+    ADC->ADC_CR1 |= (1 << EOC_INTERRUPT);
+    // Enable NVIC IRQ18
+    NVIC->ISER[ADC_IRQ / 32] |= (1 << (ADC_IRQ % 32));
+    // global enable NVIC MASK
+    _global_enable_NVIC();
     // Turn on ADC conversion
     ADC->ADC_CR2 |= 1;
+    // Enable Continous Conversion Mode
+    ADC->ADC_CR2 |= (1 << CONT_MODE);
+    // Start the conversion
+    ADC->ADC_CR2 |= (1 << CONV_START);
 
-    for(;;){
-        // Start the conversion
-        ADC->ADC_CR2 |= (1 << CONV_START);
-        // Check if Conversion has finished
-        while(!(ADC->ADC_SR & (1 << EOC_FLAG)));
-        // Read ADC value | EOC is reset automatically
-        uint16_t result = ADC->ADC_DR;
-        printf("Value %u:\r\n", (uint16_t)result);
-    }
+    for(;;);
 }
